8616a675f56df61b920cb548490726d5e4afe8a5
Sébastien <dolor_sit_amet@protonmail.ch>
2019-03-08 17:03:47 +0100

    Added side by side component

--- src/ui/hear-ye-ui-api.ts
+++ src/ui/hear-ye-ui-api.ts
@@ -1,3 +1,4 @@
+export * from './side-by-side/side-by-side';
 export * from './controls/controls';
 export * from './sink/sink';
 export * from './gallery/gallery';
---
+++ src/ui/side-by-side/side-by-side.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import './side-by-side.scss';
+
+export function SideBySide(props: React.Props<{}>) {
+  return <div className="hy-side-by-side">
+    {props.children}
+  </div>;
+}
