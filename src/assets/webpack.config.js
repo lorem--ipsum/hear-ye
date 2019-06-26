b6e545fa2fa6ec1472fdc57feeeeac229e84b272
Sébastien <dolor_sit_amet@protonmail.ch>
2019-06-26 19:35:07 +0200

    updates

--- src/assets/webpack.config.js
+++ src/assets/webpack.config.js
@@ -1,22 +1,23 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-const MuskadPlugin = require('muskad/dist/webpack-plugin/muskad');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const MuskadPlugin = require("muskad/dist/webpack-plugin/muskad");
 
-const rootDir = require(path.resolve('./tsconfig.json')).compilerOptions.rootDir;
+const rootDir = require(path.resolve("./tsconfig.json")).compilerOptions
+  .rootDir;
 
-const src = path.resolve('.', rootDir);
+const src = path.resolve(".", rootDir);
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'development',
+  mode: process.env.NODE_ENV || "development",
   context: src,
-  devtool: 'eval-cheap-module-source-map',
-  entry: './.tmp/index.tsx',
+  devtool: "eval-cheap-module-source-map",
+  entry: "./.tmp/index.tsx",
   devServer: {
-    contentBase: path.resolve('demo'),
+    contentBase: path.resolve("demo"),
     port: 1234,
-    hot: true,
+    hot: true
   },
 
   optimization: {
@@ -24,17 +25,23 @@ module.exports = {
       cacheGroups: {
         vendors: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
+          name: "vendors",
+          chunks: "all"
         },
         project: {
           test: (module, chunk) => {
-            if (module.resource && module.resource.includes('/src/.tmp/index.tsx')) return false;
+            if (
+              module.resource &&
+              module.resource.includes("/src/.tmp/index.tsx")
+            )
+              return false;
             if (/\/node_modules\//.test(module.resource)) return false;
-            return /\/src(\/[^.]+)+(\.demo\.tsx)$/.test(module.resource) === false;
+            return (
+              /\/src(\/[^.]+)+(\.demo\.tsx)$/.test(module.resource) === false
+            );
           },
-          name: 'project',
-          chunks: 'all'
+          name: "project",
+          chunks: "all"
         }
       }
     }
@@ -47,7 +54,7 @@ module.exports = {
         include: src,
         use: [
           {
-            loader: 'ts-loader',
+            loader: "ts-loader",
             options: {
               onlyCompileBundledFiles: true,
               compilerOptions: {
@@ -61,48 +68,50 @@ module.exports = {
         test: /\.scss$/,
         include: src,
         use: [
-          {loader: 'style-loader'},
-          {loader: 'css-loader'},
+          { loader: "style-loader" },
+          { loader: "css-loader" },
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
-              ident: 'postcss',
+              ident: "postcss",
               plugins: () => [
-                require('postcss-preset-env')({
-                  browsers: ['> 1%', 'last 3 versions', 'Firefox ESR', 'Opera 12.1']
+                require("postcss-preset-env")({
+                  overrideBrowserslist: [
+                    "> 1%",
+                    "last 3 versions",
+                    "Firefox ESR",
+                    "Opera 12.1"
+                  ]
                 })
               ]
             }
           },
-          {loader: 'sass-loader'}
+          { loader: "sass-loader" }
         ]
       },
       {
         test: /\.css$/,
-        use: [
-          {loader: 'style-loader'},
-          {loader: 'css-loader'}
-        ]
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
       }
     ]
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js', '.scss', '.css', '.demo.tsx' ],
+    extensions: [".tsx", ".ts", ".js", ".scss", ".css", ".demo.tsx"],
     plugins: [new TsconfigPathsPlugin({})],
     symlinks: false,
     cacheWithContext: false
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve('./demo'),
+    filename: "[name].js",
+    path: path.resolve("./demo"),
     pathinfo: false
   },
   plugins: [
     new MuskadPlugin({
-      context: path.resolve('src')
+      context: path.resolve("src")
     }),
     new HtmlWebpackPlugin({
-      template: '.tmp/index.html'
+      template: ".tmp/index.html"
     }),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
   ],
--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,82 @@
-const process = require('process');
-const CopyPlugin = require('copy-webpack-plugin');
-const nodeExternals = require('webpack-node-externals');
+const process = require("process");
+const CopyPlugin = require("copy-webpack-plugin");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = [
-{
-  target: 'node',
-  mode: process.env.NODE_ENV || 'development',
-  entry: {
-    'hear-ye': './src/hear-ye.ts'
-  },
-  module: {
-    rules: [
-      {test: /\.ts$/, use: ['ts-loader']}
-    ]
-  },
-  resolve: {
-    extensions: [ '.ts', '.js' ]
+  {
+    target: "node",
+    mode: process.env.NODE_ENV || "development",
+    entry: {
+      "hear-ye": "./src/hear-ye.ts"
+    },
+    module: {
+      rules: [{ test: /\.ts$/, use: ["ts-loader"] }]
+    },
+    resolve: {
+      extensions: [".ts", ".js"]
+    },
+
+    output: {
+      path: __dirname + "/dist",
+      filename: "[name].js",
+      library: "[name]",
+      libraryTarget: "umd",
+      umdNamedDefine: true
+    },
+
+    plugins: [new CopyPlugin([{ from: "src/assets", to: "assets" }])],
+
+    node: {
+      __dirname: false
+    },
+
+    externals: [nodeExternals()]
   },
 
-  output: {
-    path: __dirname + '/dist',
-    filename: '[name].js',
-    library: '[name]',
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
+  {
+    target: "web",
+    mode: process.env.NODE_ENV || "development",
+    entry: {
+      "hear-ye-ui-api": "./src/ui/hear-ye-ui-api.ts"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: ["ts-loader"]
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            { loader: "style-loader" },
+            { loader: "css-loader" },
+            {
+              loader: "postcss-loader",
+              options: {
+                ident: "postcss",
+                plugins: () => [require("postcss-preset-env")({})]
+              }
+            },
+            { loader: "sass-loader" }
+          ]
+        }
+      ]
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js", ".scss"]
+    },
 
-  plugins: [
-    new CopyPlugin([
-      { from: 'src/assets', to: 'assets' }
-    ])
-  ],
+    output: {
+      path: __dirname + "/dist",
+      filename: "[name].js",
+      library: "[name]",
+      libraryTarget: "umd",
+      umdNamedDefine: true
+    },
 
-  node: {
-    __dirname: false
-  },
-
-  externals: [nodeExternals()]
-},
-
-{
-  target: 'web',
-  mode: process.env.NODE_ENV || 'development',
-  entry: {
-    'hear-ye-ui-api': './src/ui/hear-ye-ui-api.ts'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: ['ts-loader'],
-      },
-      {
-        test: /\.s?css$/,
-        use: [
-          {loader: 'style-loader'},
-          {loader: 'css-loader'},
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: () => [
-                require('postcss-preset-env')({
-                  browsers: ['> 1%', 'last 3 versions', 'Firefox ESR', 'Opera 12.1']
-                })
-              ]
-            }
-          },
-          {loader: 'sass-loader'}
-        ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js', '.scss' ]
-  },
-
-  output: {
-    path: __dirname + '/dist',
-    filename: '[name].js',
-    library: '[name]',
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-
-  externals: {
-    "react": "react",
-    "react-dom": "react-dom"
+    externals: {
+      react: "react",
+      "react-dom": "react-dom"
+    }
   }
-}];
+];
